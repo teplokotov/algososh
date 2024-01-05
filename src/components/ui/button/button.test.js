b5f786f68a2a1bff5a9a9c4b00f17a1c5625abdc
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-01-05 10:10:11 +0300

    feat: add button.test

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -1,9 +1,22 @@
 import renderer from 'react-test-renderer';
 import { Button } from "./button";
 
-it('Кнопка рендерится без ошибок', () => {
-  const tree = renderer
-    .create(<Button />)
-    .toJSON();
-    expect(tree).toMatchSnapshot();
+it('Кнопка с текстом', () => {
+	const tree = renderer.create(<Button text='Test'/>).toJSON();
+	expect(tree).toMatchSnapshot();
+}); 
+
+it('Кнопка без текста', () => {
+  const tree = renderer.create(<Button />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Заблокированная кнопка', () => {
+  const tree = renderer.create(<Button disabled/>).toJSON();
+  expect(tree).toMatchSnapshot();
+}); 
+
+it('Кнопка с индикацией загрузки', () => {
+  const tree = renderer.create(<Button isLoader={true}/>).toJSON();
+  expect(tree).toMatchSnapshot();
 }); 
