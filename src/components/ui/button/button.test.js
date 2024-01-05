0e2e684da0470211e88dbde9cd96596503459b39
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-01-05 18:31:16 +0300

    fix: add describe to button.test

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -1,22 +1,24 @@
 import renderer from 'react-test-renderer';
 import { Button } from "./button";
 
-it('Кнопка с текстом', () => {
-	const tree = renderer.create(<Button text='Test'/>).toJSON();
-	expect(tree).toMatchSnapshot();
-}); 
+describe('Кнопка', () => {
+	it('с текстом', () => {
+		const tree = renderer.create(<Button text='Test'/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	}); 
 
-it('Кнопка без текста', () => {
-  const tree = renderer.create(<Button />).toJSON();
-  expect(tree).toMatchSnapshot();
+	it('без текста', () => {
+		const tree = renderer.create(<Button />).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it('заблокированная', () => {
+		const tree = renderer.create(<Button disabled/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	}); 
+
+	it('с индикацией загрузки', () => {
+		const tree = renderer.create(<Button isLoader={true}/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 });
-
-it('Заблокированная кнопка', () => {
-  const tree = renderer.create(<Button disabled/>).toJSON();
-  expect(tree).toMatchSnapshot();
-}); 
-
-it('Кнопка с индикацией загрузки', () => {
-  const tree = renderer.create(<Button isLoader={true}/>).toJSON();
-  expect(tree).toMatchSnapshot();
-}); 
