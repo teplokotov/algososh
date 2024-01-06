1c554200d5357b81562863dbd07c9ea83c86a6ee
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2024-01-06 12:59:12 +0300

    feat: add routing cypress test

---
+++ cypress/e2e/routing.spec.cy.ts
@@ -0,0 +1,106 @@
+describe('Тестирование переходов по страницам', function() {
+  
+  const addr = 'http://localhost:3000';
+
+  it('должна открываться заглавная страница', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.contains('МБОУ АЛГОСОШ');
+  });
+
+  it('должна открываться страница: Реверс строки', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(1)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Строка');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+
+  it('должна открываться страница: Последовательность Фибоначчи', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(2)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Последовательность Фибоначчи');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+
+  it('должна открываться страница: Сортировка массива', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(3)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Сортировка массива');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+
+  it('должна открываться страница: Стек', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(4)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Стек');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+
+  it('должна открываться страница: Очередь', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(5)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Очередь');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+
+  it('должна открываться страница: Связный список', function() {
+    cy.viewport(1280, 1024);
+    cy.visit(addr);
+    cy.get('#root > div > main > div > a:nth-child(6)')
+      .invoke('attr', 'href')
+      .then(href => {
+        cy.visit(addr + href);
+      });
+    cy.contains('Связный список');
+    cy.get('#root > div > main > div > a')
+    .invoke('attr', 'href')
+    .then(href => {
+      cy.visit(addr + href);
+    });  
+  });
+}); 
